92d841247e7a90f9710a52e16f088680101eb6fb
Collin Smallegan <cmsmallegan@comsma.com>
2023-09-15 17:46:05 -0500

    Changes by dprevite

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->to('/login');
 });
 
 Route::get('/dashboard', function () {
@@ -34,5 +29,27 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(['auth', 'verified'])->group(function (): void {
+    Route::get('/jobs', [\App\Http\Controllers\JobController::class, 'listJobs'])->name('jobs.list');
+    Route::post('/jobs', [\App\Http\Controllers\JobController::class, 'create'])->name('jobs.create');
+
+    Route::get('/jobs/{id}', [\App\Http\Controllers\JobController::class, 'view']);
+    Route::delete('/jobs/{id}', [\App\Http\Controllers\JobController::class, 'destroy'])->name('jobs.delete');
+
+    Route::post('/jobs/{job_id}/documents', [\App\Http\Controllers\DocumentsController::class, 'create']);
+    Route::get('/documents', [\App\Http\Controllers\DocumentsController::class, 'listDocuments']);
+
+    Route::get('/jobs/{job_id}/documents/{document_id}', [\App\Http\Controllers\DocumentsController::class, 'view']);
+    Route::delete('/jobs/{job_id}/documents/{document_id}', [\App\Http\Controllers\DocumentsController::class, 'destroy']);
+
+    Route::get('/admin/user/{id}', [\App\Http\Controllers\AdminController::class, 'editUser'])->name('admin.user.edit');
+    Route::patch('/admin/user/{id}', [\App\Http\Controllers\AdminController::class, 'updateUser'])->name('admin.user.update');
+
+
+});
+
+
+
 
 require __DIR__.'/auth.php';
